b09c693e96f1770cb57b7fa5282a16d9e22bedbe
DerpyNewbie <contact@derpynewbie.dev>
2025-02-06 23:47:57 +0900

    Improve visuals of webpage

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log('Happy developing ✨')
+
+
+async function loadLeaderboard() {
+    let response = await fetch("https://assets-1.derpynewbie.dev/api/v1/leaderboard");
+    response.json().then(records => {
+        let html = "";
+        for (let i = 0; i < records.length; i++) {
+            let record = records[i];
+            html = "<td><td>" + record["rank"] + "</td><td>" + record["name"] + "</td><td>" + record["score"] + "m</td></tr>";
+            document.getElementById("leaderboard").innerHTML = html;
+        }
+    })
+}
+
+loadLeaderboard().then(leaderboard => {console.log("Loaded leaderboard" + leaderboard)});
