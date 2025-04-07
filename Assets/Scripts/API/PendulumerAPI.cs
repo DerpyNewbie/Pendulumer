c0e591d5efa3da5a97c8e3016a44207ee1a917b8
DerpyNewbie <contact@derpynewbie.dev>
2025-04-07 20:17:42 +0900

    Fix caching issue when calling API

--- Assets/Scripts/API/PendulumerAPI.cs
+++ Assets/Scripts/API/PendulumerAPI.cs
@@ -17,9 +17,11 @@ namespace API
 
         public IEnumerator GetLeaderboard(Action<LeaderboardRecord[]> callback, Action<UnityWebRequest> onError = null)
         {
-            var uri = new Uri(_baseUri, "/api/v1/leaderboard");
-            Debug.Log($"GetLeaderboard: {uri}");
+            var uri = new Uri(_baseUri, $"/api/v1/leaderboard?t={GetUnixTime()}");
             var request = UnityWebRequest.Get(uri);
+
+            SetNoCache(request);
+
             yield return request.SendWebRequest();
             if (request.result != UnityWebRequest.Result.Success)
             {
@@ -29,15 +31,17 @@ namespace API
             else
             {
                 var result = JsonUtility.FromJson<LeaderboardResponse>(request.downloadHandler.text);
-                Debug.Log($"GetLeaderboard: {request.downloadHandler.text}");
-
                 callback.Invoke(result.records.ToArray());
             }
         }
 
         public IEnumerator GetRanking(float score, Action<int> callback, Action<UnityWebRequest> onError = null)
         {
-            var request = UnityWebRequest.Get(new Uri(_baseUri, $"/api/v1/leaderboard/ranking?score={score}"));
+            var uri = new Uri(_baseUri, $"/api/v1/leaderboard/ranking?score={score}&t={GetUnixTime()}");
+            var request = UnityWebRequest.Get(uri);
+
+            SetNoCache(request);
+
             yield return request.SendWebRequest();
 
             if (request.result != UnityWebRequest.Result.Success)
@@ -57,10 +61,12 @@ namespace API
             }
 
             var request = UnityWebRequest.Post(
-                new Uri(_baseUri, "/api/v1/results"),
+                new Uri(_baseUri, $"/api/v1/results?t={GetUnixTime()}"),
                 JsonUtility.ToJson(result),
                 "application/json"
             );
+
+            SetNoCache(request);
 
             yield return request.SendWebRequest();
 
@@ -76,13 +82,27 @@ namespace API
 
         public IEnumerator GetStats(Action<StatsRecord> callback, Action<UnityWebRequest> onError = null)
         {
-            var request = UnityWebRequest.Get(new Uri(_baseUri, "/api/v1/stats"));
+            var request = UnityWebRequest.Get(new Uri(_baseUri, $"/api/v1/stats?t={GetUnixTime()}"));
+
+            SetNoCache(request);
+
             yield return request.SendWebRequest();
 
             if (request.result != UnityWebRequest.Result.Success)
                 onError?.Invoke(request);
             else
                 callback.Invoke(JsonUtility.FromJson<StatsRecord>(request.downloadHandler.text));
+        }
+
+        private static int GetUnixTime()
+        {
+            return (int)DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalSeconds;
+        }
+
+        private static void SetNoCache(UnityWebRequest webRequest)
+        {
+            webRequest.SetRequestHeader("Cache-Control", "max-age=0, no-cache, no-store");
+            webRequest.SetRequestHeader("Pragma", "no-cache");
         }
     }
 }
